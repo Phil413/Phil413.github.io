a106b7e3659d27742464065d731c6a93e6ba3a34
renjia <renjia@starmerx.com>
2021-03-26 14:31:41 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 set -e
-hugo
-cd public
-git add -A
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-    msg="$*"
-fi
-git commit -m "$msg"
-git push origin master
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git branch -D master
+git worktree add -b master public origin/master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+env HUGO_ENV="production" hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Pushing to github"
+git push --all --force
